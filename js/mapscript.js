d6f03a68d82da3008c9d48ba3ee7e6d0a03f0d65
jvillaluz1 <jvillaluz@hawk.iit.edu>
2025-04-16 00:07:26 -0500

    Update googlemaps.js

--- js/mapscript.js
+++ js/mapscript.js
@@ -1,10 +1,46 @@
 // Script used to display the google map
 
-var map;
-
+// Initialize and add the map
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 45.6925, lng: -121.5519}
-		zoom: 8
-	});
+    // The location to center on (example: San Francisco)
+    const center = {lat: 45.6925, lng: -121.5519};
+    
+    // The map, centered at the chosen location
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: center,
+    });
+    
+    // Add a marker at the center
+    const marker = new google.maps.Marker({
+        position: center,
+        map: map,
+    });
+    
+    // You can add more map functionality here, such as:
+    
+    // Add click event listener to the map
+    map.addListener("click", (event) => {
+        // Add a marker when user clicks on the map
+        placeMarker(event.latLng, map);
+    });
+}
+
+// Function to place a marker at a location
+function placeMarker(location, map) {
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+    
+    // Optional: Create an info window for the new marker
+    const infowindow = new google.maps.InfoWindow({
+        content: "Latitude: " + location.lat().toFixed(6) + 
+                "<br>Longitude: " + location.lng().toFixed(6),
+    });
+    
+    // Open the info window when marker is clicked
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+    });
 }
